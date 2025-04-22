92e9fd3e16dcdbf4d320718a704189393645c60a
Ayush-Raj-71 <raj.ayush0304@gmail.com>
2025-04-22 19:41:17 +0530

    Intro Corrected

--- components/ui/Intro.tsx
+++ components/ui/Intro.tsx
@@ -7,7 +7,7 @@ export default function Intro() {
       <div className='mt-2 flex-1 md:mt-0 animate-fade-in-left delay-100'>
         <h1 className='title no-underline font-black text-3xl'>Hey, I&#39;m Ayush.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          Results-driven CSE student skilled in Problem Solving and Full-Stack development with NextJs,ReactJs ,TypeScript, Node.js, and MongoDB.
+          Results-driven CSE student skilled in Problem Solving and Full-Stack development with NextJs, ReactJs ,TypeScript, Node.js, and MongoDB.
         </p>
       </div>
       <div className='relative animate-fade-in-right delay-200'>
